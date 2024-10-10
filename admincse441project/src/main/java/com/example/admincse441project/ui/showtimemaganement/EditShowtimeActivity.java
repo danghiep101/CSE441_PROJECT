60710e4f4fa56336f7de32159b5dac9d6750df6c
dangh <danghiep101@gmail.com>
2024-10-10 20:16:43 +0700

    Dang Quoc Hiep update admin add edit popcorn and showtime layout

---
+++ admincse441project/src/main/java/com/example/admincse441project/ui/showtimemaganement/EditShowtimeActivity.java
@@ -0,0 +1,26 @@
+package com.example.admincse441project.ui.showtimemaganement;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.admincse441project.R;
+
+public class EditShowtimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_showtime);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
