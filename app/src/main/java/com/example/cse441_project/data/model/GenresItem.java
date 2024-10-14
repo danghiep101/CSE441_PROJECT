9c8e6a30be75c8f03b1e782e931f6b8d14893ac9
dangh <danghiep101@gmail.com>
2024-10-14 13:24:01 +0700

    DangQuocHiep update Feature display NowPlayingFragment using Retrofit call api

---
+++ app/src/main/java/com/example/cse441_project/data/model/GenresItem.java
@@ -0,0 +1,14 @@
+package com.example.cse441_project.data.model;
+
+public class GenresItem{
+	private String name;
+	private int id;
+
+	public String getName(){
+		return name;
+	}
+
+	public int getId(){
+		return id;
+	}
+}
--- app/src/main/java/com/example/cse441_project/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/cse441_project/ui/home/HomeActivity.java
@@ -9,14 +9,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.cse441_project.R;
+import com.example.cse441_project.databinding.ActivityHomeBinding;
+import com.example.cse441_project.ui.home.nowplaying.NowPlayingFragment;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnNowplaying.setOnClickListener(v -> {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, new NowPlayingFragment())
+                        .commit();
+        });
+
+
 
     }
 }
