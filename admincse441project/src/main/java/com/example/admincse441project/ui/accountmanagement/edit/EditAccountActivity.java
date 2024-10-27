5336f76b71615ce027f47a539f658e8ea59cd06e
Cù Thịnh <thinh060903@gmail.com>
2024-10-27 20:05:15 +0700

    Update edit account

---
+++ admincse441project/src/main/java/com/example/admincse441project/ui/accountmanagement/edit/EditAccountActivity.java
@@ -0,0 +1,30 @@
+package com.example.admincse441project.ui.accountmanagement.edit;
+
+import android.os.Bundle;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.admincse441project.R;
+
+public class EditAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_account);
+    }
+}
